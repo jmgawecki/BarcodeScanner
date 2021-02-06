c4136e561cc7e6ba0ff171edf6b847c1070c490e
Jakub Gawecki <71930261+jmgawecki@users.noreply.github.com>
2021-02-06 15:18:51 +0000

    Initial Commit

---
+++ Shared/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Shared
+//
+//  Created by Jakub Gawecki on 06/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
